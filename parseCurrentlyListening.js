b547b40e3d680cc150aa42e4079e7a233b7092e9
Noah Holt <noahryanholt@gmail.com>
2023-01-28 19:41:13 -0600

    added censoring of bad words to currently listening

--- parseCurrentlyListening.js
+++ parseCurrentlyListening.js
@@ -1,4 +1,8 @@
+const Filter = require('bad-words');
+
 const parseCurrentlyListening = (data) => {
+    const censor = new Filter();
+
     if (!data.is_playing) {
         return {
             isPlaying: false,
@@ -7,9 +11,9 @@ const parseCurrentlyListening = (data) => {
 
     const type = data.currently_playing_type;
     if (type === 'track') {
-        const artists = data.item.artists.map((artist) => artist.name);
-        const album = data.item.album.name;
-        const track = data.item.name;
+        const artists = data.item.artists.map((artist) => censor.clean(artist.name));
+        const album = censor.clean(data.item.album.name);
+        const track = censor.clean(data.item.name);
         return {
             isPlaying: true,
             artists,
@@ -18,8 +22,8 @@ const parseCurrentlyListening = (data) => {
             type,
         }
     } else if (type === 'episode') {
-        const podcast = data.item.show.name;
-        const episode = data.item.name;
+        const podcast = censor.clean(data.item.show.name);
+        const episode = censor.clean(data.item.name);
         return {
             isPlaying: true,
             podcast,
